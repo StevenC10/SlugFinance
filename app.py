23c4f6fc7e3a08474568e1475c34f8d65a4b8d1f
Steven Chen <asfsteven@gmail.com>
2023-01-25 21:10:59 -0800

    changed swagger to yaml, added a API CALL

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 from flask_swagger_ui import get_swaggerui_blueprint
 from flask import Flask
-
+import json
 
 app = Flask(__name__)
 
+with open("stockdata.json", "r") as f:
+    arr = json.load(f)
 @app.route('/profile')
 def my_profile() :
     response_body = {
         'name' : 'Steven'
     }
-    return response_body
+    return stockArr
+@app.route('/v0/ticker/<id>')
+def getStock(id):
+    return arr
+
 
 SWAGGER_URL = '/swagger'
-API_URL = '/static/swagger.json'
+API_URL = '/static/swagger.yaml'
 SWAGGER_BLUEPRINT = get_swaggerui_blueprint (
     SWAGGER_URL,
     API_URL,
