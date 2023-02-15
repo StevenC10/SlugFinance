18eaacb50cd9c62f924b912b6c1e8b48cb47a83c
Patrick Chen <patdotchen@gmail.com>
2023-02-14 23:43:57 -0800

    log in and sign up pages update

--- frontend/react/src/components/Login.jsx
+++ frontend/react/src/components/Login.jsx
@@ -1,24 +1,44 @@
 import React from "react";
-  
+import Logo from "../images/test2.png"
+import Background from "../images/backg.jpg"
+
 const Login = () => {
   return (
-    <div className="flex flex-col w-full max-w-md p-12 space-y-4 text-center dark:bg-gray-900 dark:text-gray-100">
-      <h1 className="text-3xl font-semibold">Slug Finance - Sign In</h1>
-      <form novalidate="" className="space-y-4 ng-untouched ng-pristine ng-valid">
-        <div className="flex flex-col">
-          <label for="email" className="sr-only">Email address</label>
-          <input id="email" type="email" placeholder="Email address" className="rounded-t-md dark:border-gray-600 dark:bg-gray-900 dark:text-gray-100 focus:ring-violet-400 focus:dark:border-violet-400 focus:ring-2" />
-          <label for="password" className="sr-only">Password</label>
-          <input id="password" type="text" placeholder="Password" className="-mt-1 rounded-b-md dark:border-gray-600 dark:bg-gray-900 dark:text-gray-100 focus:ring-violet-400 focus:dark:border-violet-400 focus:ring-2" />
+    <div class="flex items-center min-h-screen bg-gray-800">
+        <div class="container mx-auto">
+            <div class="max-w-md my-0 mx-0">
+              <img class="absolute top-0 right-0 flex h-screen" src={Background} alt="back"></img>
+              <section class="hero container mx-auto pb-0 flex justify-center">
+                <img src={Logo} class="w-1/4" alt="logo" />
+              </section>
+                <div class="text-center">
+                    <h1 class="my-3 text-3xl font-semibold text-gray-700 dark:text-gray-200">Slug Finance</h1>
+                    <p class="text-gray-500 dark:text-gray-400">Sign in to access your account</p>
+                </div>
+                <div class="m-7">
+                    <form action="">
+                        <div class="mb-6">
+                            <label for="email" class="block mb-2 text-sm text-gray-600 dark:text-gray-400">Email Address</label>
+                            <input type="email" name="email" id="email" placeholder="user@ucsc.edu" class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" />
+                        </div>
+                        <div class="mb-6">
+                            <div class="flex justify-between mb-2">
+                                <label for="password" class="text-sm text-gray-600 dark:text-gray-400">Password</label>
+                                <a href="#!" class="text-sm text-gray-400 focus:outline-none focus:text-indigo-500 hover:text-indigo-500 dark:hover:text-indigo-300">Forgot password?</a>
+                            </div>
+                            <input type="password" name="password" id="password" placeholder="••••••••••" class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" />
+                        </div>
+                        <div className="flex justify-between">
+                          <div className="form-check">
+                            <input type="checkbox" name="remember" id="remember" aria-label="Remember me" className="mr-1 rounded-sm focus:ring-violet-400 focus:dark:border-violet-400 focus:outline-none accent-violet-400" />
+                            <label for="remember" className="form-check-label inline-block text-gray-400">Remember me</label>
+                          </div>
+                          <button type="button" className="bg-violet-500 hover:bg-violet-600 active:bg-violet-700 px-8 py-3 space-x-2 font-semibold rounded text-white">Sign in</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-        <div className="flex justify-between">
-          <div className="flex items-center">
-            <input type="checkbox" name="remember" id="remember" aria-label="Remember me" className="mr-1 rounded-sm focus:ring-violet-400 focus:dark:border-violet-400 focus:ring-2 accent-violet-400" />
-            <label for="remember" className="text-sm dark:text-gray-400">Remember me</label>
-          </div>
-        </div>
-        <button type="button" className="px-8 py-3 space-x-2 font-semibold rounded dark:bg-violet-400 dark:text-gray-900">Sign in</button>
-      </form>
     </div>
   )
 };
