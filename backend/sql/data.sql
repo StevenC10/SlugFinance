eb0c7c25ac1624b17a483b1c1c003a0386f84d26
Steven Chen <asfsteven@gmail.com>
2023-02-16 21:51:49 -0800

    new api calls

---
+++ backend/sql/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO dummy (created) VALUES (current_timestamp);
+INSERT INTO emailtable(personemail, personpassword) VALUES ('partickchen@ucsc.edu', 'partick');
+INSERT INTO emailtable(personemail, personpassword) VALUES ('lanceweenie@ucsc.edu', 'lance');
+INSERT INTO emailtable(personemail, personpassword) VALUES ('edwin@ucsc.edu', 'edwin');
+INSERT INTO emailtable(personemail, personpassword) VALUES ('ben@ucsc.edu', 'ben');
+INSERT INTO emailtable(personemail, personpassword) VALUES ('steven@ucsc.edu', 'steven');
+
+INSERT INTO stockTable(personemail, ticker,price, change, percentChange) VALUES ('ben@ucsc.edu', 'TSLA', 84.69,'-1.22', '(-1.42%)');
