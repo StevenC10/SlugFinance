4eaef3f30cdfc15ef95c6d4dbf4064ed4f0f6c24
edchwong <73096416+edchwong@users.noreply.github.com>
2023-03-10 01:57:39 -0800

    Update Home.jsx

--- frontend/react/src/components/Home.jsx
+++ frontend/react/src/components/Home.jsx
@@ -1,34 +1,290 @@
 
-import React, {useState,useEffect} from 'react';
+import React, {useState,useEffect,useCallback} from 'react';
 import {FaSistrix, FaBars} from "react-icons/fa"
 import Logo from '../images/test2.png'
 import ReactApexChart from "react-apexcharts";
 //import { createChartData } from './Chartdata';
-//import {useRef} from 'react;
+import {useRef} from 'react';
 
 export const tickerContext = React.createContext();
 
 const Home = () => {
-  
-  const [navbarOpen, setNavbarOpen] = React.useState(false);
-  const [c1Data, setC1Data] = useState([]);
-  const [c2Data, setC2Data] = useState([]);
-  const [c3Data, setC3Data] = useState([]);
-  const [c4Data, setC4Data] = useState([]);
-  const [c5Data, setC5Data] = useState([]);
-
   const today = new Date();
   const day = today.getDate();
   const month = today.getMonth()+1;
   const year = today.getFullYear();
+  const yesterday = new Date(Date.now() - 86400000);
+
+  const [navbarOpen, setNavbarOpen] = React.useState(false);
+  const [c1Data, setC1Data] = useState([]);
+  const [c2Data, setC2Data] = useState([]);
+  const [Icon1,setIcon1] = useState([]);
+  const iconBody = useRef({ticker: '', year: yesterday.getFullYear(), month: yesterday.getMonth()+1, day: yesterday.getDate(), year2: today.getFullYear(), month2: today.getMonth()+1, day2: today.getDate(), interval: "1d"});
+  //const newIcon = useRef({});
+   
+  const c1Options = {};
+  c1Options.series = [];
+  const c1GraphData = {'data': []}
+  c1Options.series.push(c1GraphData);
+
+  const getTickerData = useCallback(async (tickerName)=> {
+    
+    let temp=iconBody.current;
+    temp.ticker= tickerName
+    //console.log(temp);
+    //console.log(iconBody);
+    let tempData = await fetch(`http://127.0.0.1:5000/v0/getHistory`, {
+      method: 'POST',
+      body: JSON.stringify(temp),
+      headers: new Headers({
+        'Content-Type': 'application/json',
+        })
+    }).then((response)=>{
+      if(!response.ok) {
+        throw response;
+      }
+      fetch(`http://127.0.0.1:5000/v0/view?id=`+tickerName, {
+        method: 'GET',
+        headers: new Headers({
+          'Content-Type': 'application/json',
+          })
+        }).then((response1) => {
+          if(!response1.ok) {
+            throw response1;
+          }
+          return response1.json();
+        }).then((json)=>{
+          //newIcon.current=json;
+          setC2Data(json);
+          //console.log(newIcon.current);
+          return(json);
+        })
+      })
+    
+    return tempData;
+      //console.log(c2Data);
+  }, [iconBody,setC2Data])
+
+  const icon1= useRef([]);
+  const icon2= useRef([]);
+  /*const icon3= useRef([]);
+  const icon4= useRef([]);
+  const icon5= useRef([]);
+  const icon6= useRef([]);*/
+    
+ 
+  //CHART 1
+  useEffect(()=> {
+    fetch(`http://127.0.0.1:5000/v0/getHistory`, {
+      method: 'POST',
+      body: `{"ticker": "TSLA",`
+        + `"year": 2023,`
+        + `"month": 2,`
+        + `"day": 1,`
+        + `"year2": `+year+`,`
+        + `"month2": `+month+`,`
+        + `"day2": `+day+`,`
+        + `"interval": "1d"`
+        +"}",
+      headers: new Headers({
+        'Content-Type': 'application/json',
+        })
+    })
+    
+  fetch(`http://127.0.0.1:5000/v0/view?id=TSLA`, {
+    method: 'GET',
+    headers: new Headers({
+      'Content-Type': 'application/json',
+      })
+    }).then((response) => {
+      if(!response.ok) {
+        throw response;
+      }
+      return response.json();
+    }).then((json)=>{
+      setC1Data(json);
+      return(json);
+    })
+    //console.log(c1d);
   
+    getTickerData("NVDA");
+    
+    
+    getTickerData("TSLA");
+    //icon2.current=newIcon.current;
+    
+    console.log(icon1.current[0]);
+    console.log(icon1); 
+    
+    return() =>{
+      //AbortController.abort();
+    };
+  },[day,month,year,getTickerData]);
+
+  if (c1Data.length >= 1){
+    let c1History= c1Data[0][0][1];
+
+    for (let i=c1History.length-1; i>=0; i--){
+      const temp = {};
+        temp.x = new Date (c1History[i].day);
+        temp.y = [];
+        temp.y.push(c1History[i].open);
+        temp.y.push(c1History[i].high);
+        temp.y.push(c1History[i].low);
+        temp.y.push(c1History[i].close);
+        c1Options.series[0].data.push(temp);
+    }
+    c1Options.chart = {};
+    c1Options.chart.type = 'candlestick';
+    c1Options.chart.height = 350;
+    c1Options.title = {};
+    c1Options.title.text = 'TSLA';
+    c1Options.title.align = 'left';
+    c1Options.xaxis = {};
+    c1Options.xaxis.type = 'numeric';
+    c1Options.yaxis = {};
+    c1Options.yaxis.tooltip = {};
+    c1Options.yaxis.tooltip.enabled = true;
+
+    
+  }
+  
+  
+  //RENDER WEBPAGE
+  return (
+    <div className="overflow-auto">
+      <nav className="overflow-hidden relative flex items-center justify-between px-2 py-3 bg-[#27a5f8] sticky top-0">
+        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+          <img className="flex inline-block" src={Logo} width='40' height='40' alt=''/>
+          <div className="inline-block w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+            <a
+              className="text-sm flex flex-col font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+              href="#SLUGFINANCE"
+            >
+              SLUG FINANCE 
+            </a>
+            <button
+              className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+              type="button"
+              onClick={() => setNavbarOpen(!navbarOpen)}
+            >
+              <i><FaBars/></i>
+            </button>
+          </div>
+          <div className="flex flex-grow inline-block">
+            <form className="flex flex-grow" style={{background:'white', margin:'2px'}} >
+              <input className="flex flex-grow focus:outline-none" placeholder="TSLA, AAPL, NVDA..."
+              type = "text" 
+              name = "name"
+              />
+              <button className="flex-end" type="submit" > <FaSistrix/></button>
+            </form>
+          </div>
+          <div
+            className={
+              "lg:flex flex-grow inline-block items-center" +
+              (navbarOpen ? " flex" : " hidden")
+            }
+            id="example-navbar-danger"
+          >
+            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+              { <li className="nav-item">
+                <a
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  href="#SLUGFINANCE"
+                >
+                  Watchlist
+                </a>
+              </li> }
+              <li className="nav-item">
+                <a
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  href="signup"
+                >
+                  Sign Up
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  href="login"
+                >
+                  Log In
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      
+      <div className="box-border text-6xl h-screen " 
+        style={{backgroundImage: 'url(https://knowledge.wharton.upenn.edu/wp-content/uploads/2020/11/Stock-Market-900x387.jpg)',
+                    backgroundSize: "cover",
+                    align: "center",
+                    opacity: "0.85",
+                    height:1000
+                    }}>
+        <div className="grid w-1/2 h-screen"style={{paddingLeft:"60px"}}>
+          <div className="grid content-end text-yellow-300 font-serif font-mono font-bold">
+            The price is what you pay, value is what you get.
+            <p className="pt-3 content-end text-stone-400 text-3xl">
+            Always an informed investment decision. First you prepare, then you go for it.
+            </p>
+          </div>
+          
+          <form className="nav-item" style={{paddingTop:'10px'}} action="Individual" >
+              <input className="flex w-full focus:outline-none" placeholder="TSLA, AAPL, NVDA..."
+              type = "text" 
+              name = "name"
+              />
+            </form>
+        </div>
+      </div>
+      
+      <div className="pt-4 pl-4 text-6xl">
+        Market Trends
+        <div className="flex pt-4 gridgrid-row-4 grid-flow-col-dense h-screen" id="chart">
+          <div className="pr-3 text-lg row-span-4 row-start-1 w-2/3">
+            <ReactApexChart options={c1Options} series={c1Options.series} type="candlestick" height={800}/>
+          </div>
+          <div className="px-3 row-start-1 w-1/6">
+            
+            {/*<ReactApexChart options={c2Options} series={c2Options.series} type="line" height={300} width={400}/>*/}
+          </div>
+          <div className="px-3 row-start-1 w-1/6">
+
+            {/*<ReactApexChart options={c3Options} series={c3Options.series} type="line" height={300} width={400}/> */}
+          </div>
+          <div className="px-3 row-start-2 w-1/6">
+            
+            {/*<ReactApexChart options={c4Options} series={c4Options.series} type="line" height={300} width={400}/>*/}
+          </div>
+          <div className="px-3 row-start-2 w-1/6">
+            
+            {/*<ReactApexChart options={c5Options} series={c5Options.series} type="line" height={300} width={400}/>*/}
+          </div>
+        </div>
+      </div>         
+
+      <footer className="bg-[#27a5f8] h-48 px-12 pt-5" >
+        <div className="flex bold text-4xl">About SlugFinance</div>
+        <div className="flex text-2xl">We are a team of students studying computer science at University of California Santa Cruz.</div>
+
+      </footer>
+      
+      
+    </div>
+  );
+};
+  
+export default Home;
+
+
   //let effectRan=false;
   //const [chartData,setChartData] = useState({});
-  //const effectRan = useRef(false);
+  
   //chartData.info=[];
 
-  
-  
   /* modularizing the graph data code isn't working out, come back later
  for(let i=0; i<1; i++){}
         const f = async()=>{
@@ -53,75 +309,29 @@ const Home = () => {
         //console.log(chartOptions);
 
 */
-  //CHART 1
-useEffect(()=> {
-  ( async()=>{
-      await fetch(`http://127.0.0.1:5000/v0/getHistory`, {
-        method: 'POST',
-        body: `{"ticker": "TSLA",`
-              + `"year": 2023,`
-              + `"month": 2,`
-              + `"day": 1,`
-              + `"year2": `+year+`,`
-              + `"month2": `+month+`,`
-              + `"day2": `+day+`,`
-              + `"interval": "1d"`
-              +"}",
-        headers: new Headers({
-          'Content-Type': 'application/json',
-          })
-      })
-      
-      let c1d = await fetch(`http://127.0.0.1:5000/v0/view?id=TSLA`, {
-        method: 'GET',
-        headers: new Headers({
-          'Content-Type': 'application/json',
-          })
-        }).then((response) => {
-          if(!response.ok) {
-            throw response;
-          }
-          return response.json();
-        })
-      setC1Data(c1d);
-        
-  }) ();
-  return() =>{
-    //AbortController.abort();
-  };
-},[day,month,year]);
 
+/*
+const getStock = (symbol, setStockData) => {
+  //how to stop firing on intial render 
+  //https://stackoverflow.com/questions/72146986/useeffect-firing-on-initial-render
+  symbol && fetch(`http://127.0.0.1:5000/v0/ticker?id=` + symbol, {
+  method: 'GET',
+  headers: new Headers({
+    'Content-Type': 'application/x-www-form-urlencoded',
+  }),
+})
+  .then((response) => {
+    if(!response.ok) {
+      throw response;
+    }
+    return response.json();
+  })
+  .then((json) => {
+    setStockData(json)
+  })
+}*/
 
-const c1Options = {};
-c1Options.series = [];
-const c1GraphData = {'data': []}
-c1Options.series.push(c1GraphData);
-if (c1Data.length >= 1){
-  let c1History= c1Data[0][0][1];
-
-  for (let i=c1History.length-1; i>=0; i--){
-    const temp = {};
-      temp.x = new Date (c1History[i].day);
-      temp.y = [];
-      temp.y.push(c1History[i].open);
-      temp.y.push(c1History[i].high);
-      temp.y.push(c1History[i].low);
-      temp.y.push(c1History[i].close);
-      c1Options.series[0].data.push(temp);
-  }
-  c1Options.chart = {};
-  c1Options.chart.type = 'candlestick';
-  c1Options.chart.height = 350;
-  c1Options.title = {};
-  c1Options.title.text = 'TSLA';
-  c1Options.title.align = 'left';
-  c1Options.xaxis = {};
-  c1Options.xaxis.type = 'numeric';
-  c1Options.yaxis = {};
-  c1Options.yaxis.tooltip = {};
-  c1Options.yaxis.tooltip.enabled = true;
-}
-
+/*
 
   //CHART 2
   useEffect(()=> {
@@ -397,145 +607,5 @@ if (c1Data.length >= 1){
     c5Options.yaxis.tooltip.enabled = true;
     
   }
-
-  // search bar ticker context
-  
-  //RENDER WEBPAGE
-  return (
-    <div className="overflow-auto">
-      <nav className="overflow-hidden relative flex items-center justify-between px-2 py-3 bg-[#27a5f8] sticky top-0">
-        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
-          <img className="flex inline-block" src={Logo} width='40' height='40' alt=''/>
-          <div className="inline-block w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            <a
-              className="text-sm flex flex-col font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
-              href="#SLUGFINANCE"
-            >
-              SLUG FINANCE 
-            </a>
-            <button
-              className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
-              type="button"
-              onClick={() => setNavbarOpen(!navbarOpen)}
-            >
-              <i><FaBars/></i>
-            </button>
-          </div>
-          <div className="flex flex-grow inline-block">
-            <form className="flex flex-grow" style={{background:'white', margin:'2px'}} >
-              <input className="flex flex-grow focus:outline-none" placeholder="TSLA, AAPL, NVDA..."
-              type = "text" 
-              name = "name"
-              />
-              <button className="flex-end" type="submit" > <FaSistrix/></button>
-            </form>
-          </div>
-          <div
-            className={
-              "lg:flex flex-grow inline-block items-center" +
-              (navbarOpen ? " flex" : " hidden")
-            }
-            id="example-navbar-danger"
-          >
-            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
-              { <li className="nav-item">
-                <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                  href="#SLUGFINANCE"
-                >
-                  Watchlist
-                </a>
-              </li> }
-              <li className="nav-item">
-                <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                  href="signup"
-                >
-                  Sign Up
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                  href="login"
-                >
-                  Log In
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-      
-      <div className="box-border text-6xl h-screen " 
-        style={{backgroundImage: 'url(https://knowledge.wharton.upenn.edu/wp-content/uploads/2020/11/Stock-Market-900x387.jpg)',
-                    backgroundSize: "cover",
-                    align: "center",
-                    height:1000
-                    }}>
-        <div className="grid grid-rows-2 w-2/3 h-screen"style={{paddingLeft:"60px"}}>
-          <p className="grid content-end text-yellow-200 font-serif font-mono font-bold">
-            The price is what you pay, value is what you get.
-          </p>
-          <form className="nav-item" style={{paddingTop:'10px'}} action="Individual" >
-              <input className="flex w-full focus:outline-none" placeholder="TSLA, AAPL, NVDA..."
-              type = "text" 
-              name = "name"
-              />
-            </form>
-        </div>
-        
-      </div>
-      
-      <div className="flex grid grid-col-4 grid-row-4 grid-flow-col-dense h-screen" id="chart">
-        <div className="px-3 row-span-4 col-span-2 row-start-1">
-          <ReactApexChart options={c1Options} series={c1Options.series} type="candlestick" height={800} width={850}/>
-        </div>
-        <div className="px-3 row-start-1">
-          
-          <ReactApexChart options={c2Options} series={c2Options.series} type="line" height={300} width={400}/>
-        </div>
-        <div className="px-3 row-start-1">
-          <ReactApexChart options={c3Options} series={c3Options.series} type="line" height={300} width={400}/>
-        </div>
-        <div className="px-3 row-start-2">
-          
-          <ReactApexChart options={c4Options} series={c4Options.series} type="line" height={300} width={400}/>
-        </div>
-        <div className="px-3 row-start-2">
-          
-          <ReactApexChart options={c5Options} series={c5Options.series} type="line" height={300} width={400}/>
-        </div>
-      </div>
-      <footer className="bg-[#27a5f8] h-48 px-12 pt-5" >
-        <div className="flex bold text-4xl">About SlugFinance</div>
-        <div className="flex text-2xl">We are a team of students studying computer science at University of California Santa Cruz.</div>
-
-      </footer>
-      
-      
-    </div>
-  );
-};
-  
-export default Home;
-/*
-const getStock = (symbol, setStockData) => {
-  //how to stop firing on intial render 
-  //https://stackoverflow.com/questions/72146986/useeffect-firing-on-initial-render
-  symbol && fetch(`http://127.0.0.1:5000/v0/ticker?id=` + symbol, {
-  method: 'GET',
-  headers: new Headers({
-    'Content-Type': 'application/x-www-form-urlencoded',
-  }),
-})
-  .then((response) => {
-    if(!response.ok) {
-      throw response;
-    }
-    return response.json();
-  })
-  .then((json) => {
-    setStockData(json)
-  })
-}*/
+  */
+ 
