204b1b5d491de8b966f2404f012bf56c60c0aa02
lddelacr <123709657+lddelacr@users.noreply.github.com>
2023-03-13 21:14:48 -0700

    Styled

--- frontend/react/src/components/Portfolio.jsx
+++ frontend/react/src/components/Portfolio.jsx
@@ -175,49 +175,49 @@ const Portfolio = () => {
   }
 
   return (
-	<div>
-    {/* navbar start */}
-    <header className="top-0 sm:px-12 mx-auto flex items-center p-4 bg-blue-900">
-      <div className="container flex justify-between h-10 mx-auto">
-        <a rel="noopener noreferrer" href="/" aria-label="Back to homepage" className="flex items-center p-2 mx-0">
-          <img src={Logo} className="w-12 h-12" alt="logo" />
-          <div className="text-xl flex flex-col font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-gray-200">
-              SLUG FINANCE 
+    <div>
+      {/* navbar start */}
+      <header className="top-0 sm:px-12 mx-auto flex items-center p-4 bg-blue-900">
+        <div className="container flex justify-between h-10 mx-auto">
+          <a rel="noopener noreferrer" href="/" aria-label="Back to homepage" className="flex items-center p-2 mx-0">
+            <img src={Logo} className="w-12 h-12" alt="logo" />
+            <div className="text-xl flex flex-col font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-gray-200">
+                SLUG FINANCE 
+            </div>
+          </a>
+          <div className="mb-6 items-center justify-start">
+            <form action="Individual">
+              <input type="text" name="name" id="search" placeholder="TSLA, AAPL, NVDA" className="w-full px-4 py-2 placeholder-gray-500 border border-gray-200 rounded-md focus:outline-none bg-gray-20 border-gray-600" />
+            </form>
           </div>
-        </a>
-        <div className="mb-6 items-center justify-start">
-          <form action="Individual">
-            <input type="text" name="name" id="search" placeholder="TSLA, AAPL, NVDA" className="w-full px-4 py-2 placeholder-gray-500 border border-gray-200 rounded-md focus:outline-none bg-gray-20 border-gray-600" />
-          </form>
+          <div className="items-center space-x-2 flex-shrink-0 hidden lg:flex">
+            <li className="flex">
+              <button type="button" className="px-8 py-3 font-semibold rounded-full bg-gray-500 text-gray-800" onClick={toPortfolio}>myPortfolio</button>
+            </li>
+            <button className="self-center px-8 py-3 rounded text-gray-200 bg-blue-600 hover:bg-blue-700 font-semibold active:bg-blue-800" onClick={toSignup}>Sign up</button>
+            <button className="self-center px-8 py-3 rounded text-gray-200 bg-gray-600 hover:bg-gray-700 font-semibold active:bg-gray-800" onClick={toLogin}>Log in</button>
+          </div>
         </div>
-        <div className="items-center space-x-2 flex-shrink-0 hidden lg:flex">
-          <li className="flex">
-            <button type="button" className="px-8 py-3 font-semibold rounded-full bg-gray-500 text-gray-800" onClick={toPortfolio}>myPortfolio</button>
-          </li>
-          <button className="self-center px-8 py-3 rounded text-gray-200 bg-blue-600 hover:bg-blue-700 font-semibold active:bg-blue-800" onClick={toSignup}>Sign up</button>
-          <button className="self-center px-8 py-3 rounded text-gray-200 bg-gray-600 hover:bg-gray-700 font-semibold active:bg-gray-800" onClick={toLogin}>Log in</button>
-        </div>
+      </header>
+      {/* navbar end */}
+      <h2 className="m-4 text-2xl font-semibold leading-tight">Watchlist</h2>
+      <div className="overflow-hidden rounded-lg border border-gray-200 shadow-md m-5">
+        <table className="w-full border-collapse bg-white text-left text-sm text-gray-500">
+          <thead className="bg-gray-100">
+            <tr>
+              <th scope="col" className="px-6 py-4 font-medium text-gray-900">Ticker</th>
+              <th scope="col" className="px-6 py-4 font-medium text-gray-900">Change (Daily)</th>
+              <th scope="col" className="px-6 py-4 font-medium text-gray-900">Change % (Daily)</th>
+              <th scope="col" className="px-6 py-4 font-medium text-gray-900">Last Price</th>
+              <th scope="col" className="px-6 py-4 font-medium text-gray-900"></th>
+            </tr>
+          </thead>
+          <tbody className="divide-y divide-gray-100 border-t border-gray-100">
+            {rows}
+          </tbody>
+        </table>
       </div>
-    </header>
-    {/* navbar end */}
-	<h2 className="m-4 text-2xl font-semibold leading-tight">Watchlist</h2>
-  <div className="overflow-hidden rounded-lg border border-gray-200 shadow-md m-5">
-  	<table className="w-full border-collapse bg-white text-left text-sm text-gray-500">
-      <thead className="bg-gray-100">
-        <tr>
-          <th scope="col" className="px-6 py-4 font-medium text-gray-900">Ticker</th>
-          <th scope="col" className="px-6 py-4 font-medium text-gray-900">Change (Daily)</th>
-          <th scope="col" className="px-6 py-4 font-medium text-gray-900">Change % (Daily)</th>
-          <th scope="col" className="px-6 py-4 font-medium text-gray-900">Last Price</th>
-          <th scope="col" className="px-6 py-4 font-medium text-gray-900"></th>
-        </tr>
-      </thead>
-      <tbody className="divide-y divide-gray-100 border-t border-gray-100">
-        {rows}
-      </tbody>
-    </table>
-  </div>
-</div>
+    </div>
   )
 };
   
