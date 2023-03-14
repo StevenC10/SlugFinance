66abe231676ff67a61be218b639bea9736da1c0d
edchwong <73096416+edchwong@users.noreply.github.com>
2023-03-14 16:27:01 -0700

    Update Home.jsx

--- frontend/react/src/components/Home.jsx
+++ frontend/react/src/components/Home.jsx
@@ -1,41 +1,59 @@
 
 import React, {useState,useEffect} from 'react';
 import ReactApexChart from "react-apexcharts";
-import {useRef} from 'react';
+//import {useRef} from 'react';
 import Navbar from './Navbar';
 
 import Amazon from '../images/Amazon.png';
-import Apple from '../images/Apple.png';
+import CME from '../images/CMEGroup.png';
 import Google from '../images/Google.png';
 import Microsoft from '../images/MSFT.png';
-import Meta from '../images/Meta.png';
-import Netflix from '../images/Netflix.png';
+import DOW from '../images/DowJones.png';
+import Nasdaq from '../images/Nasdaq.png';
 
 // import homeBackground from '../images/homeImage1.jpg';
 
 export const tickerContext = React.createContext();
 
+
+/*
+const today = new Date();
+const year = today.getFullYear();
+const month = today.getMonth()+1;
+const day = today.getDate();
+//const baseDay = `"`+year+`-`+month+`-`+day;
+//const yesterday = new Date(Date.now() - 432000000);
+*/
+
+
+
 const Home = () => {
-  const today = new Date();
-  const year = today.getFullYear();
-  const month = today.getMonth()+1;
-  const day = today.getDate();
-  const baseDay = `"`+year+`-`+month+`-`+day;
-  const yesterday = new Date(Date.now() - 432000000);
+  
+  //const iconBody = useRef({ticker: '', year: yesterday.getFullYear(), month: yesterday.getMonth()+1, day: yesterday.getDate(), year2: year, month2: month, day2: day, interval: "1d"});
   const [c1Data, setC1Data] = useState({});
-  const [icon1, setIcon1] = useState([[["",[{high:0,close:0,low:0,open:0,day:baseDay}]]]]);
-  const [icon2, setIcon2] = useState([[["",[{high:0,close:0,low:0,open:0,day:baseDay}]]]]);
-  const [icon3, setIcon3] = useState([[["",[{high:0,close:0,low:0,open:0,day:baseDay}]]]]);
-  const [icon4, setIcon4] = useState([[["",[{high:0,close:0,low:0,open:0,day:baseDay}]]]]);
-  const [icon5, setIcon5] = useState([[["",[{high:0,close:0,low:0,open:0,day:baseDay}]]]]);
-  const [icon6, setIcon6] = useState([[["",[{high:0,close:0,low:0,open:0,day:baseDay}]]]]);
-  const iconBody = useRef({ticker: '', year: yesterday.getFullYear(), month: yesterday.getMonth()+1, day: yesterday.getDate(), year2: year, month2: month, day2: day, interval: "1d"});
+  const [icon1, setIcon1] = useState([[["","","0","0","0"]]]);
+  const [icon2, setIcon2] = useState([[["","","0","0","0"]]]);
+  const [icon3, setIcon3] = useState([[["","","0","0","0"]]]);
+  const [icon4, setIcon4] = useState([[["","","0","0","0"]]]);
+  const [icon5, setIcon5] = useState([[["","","0","0","0"]]]);
+  const [icon6, setIcon6] = useState([[["","","0","0","0"]]]);
+
 
   useEffect(()=> {
-    const fetchData = async () => { 
-      let tempDate=new Date(Date.now()-7890000000);
-      let tempToday = new Date();
-  
+    let tempDate=new Date(Date.now()-7890000000);
+    let tempToday= new Date();
+
+    // Chart for Tesla 
+    fetch(`http://127.0.0.1:5000/v0/add`, {
+      method: 'POST',
+      body: `{"ticker": "TSLA"}`,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then((add) =>{
+      if (!add.ok) {
+        throw add;
+      }
       fetch(`http://127.0.0.1:5000/v0/getHistory`, {
         method: 'POST',
         body: `{"ticker": "TSLA",`
@@ -47,138 +65,215 @@ const Home = () => {
           + `"day2": `+tempToday.getDate()+`,`
           + `"interval": "1d"`
           +"}",
-        headers: new Headers({
+        headers: {
           'Content-Type': 'application/json',
-        })
-      });
-      
-      const chartResponse = await fetch(`http://127.0.0.1:5000/v0/view?id=TSLA`, {
-        method: 'GET',
-        headers: new Headers({
-          'Content-Type': 'application/json',
-        })
-      });
-      const chartJson = await chartResponse.json();
-      console.log(chartJson)
-      setC1Data(chartJson);
-      
-
+        },
+      }).then((res) => {
+          if(!res.ok){
+            throw res;
+          }
+          fetch(`http://127.0.0.1:5000/v0/view?id=TSLA`, {
+          method: 'GET',
+          headers: new Headers({
+            'Content-Type': 'application/x-www-form-urlencoded',
+          })
+          })
+          .then((response) => {
+            return response.json();
+          })
+          .then((json) => {
+            setC1Data(json);
+            return json;
+          })
+        });
+      }   
+    )
+    
+    
       // Icon 1
-      let temp=iconBody.current;
-      temp.ticker="META";
-
-      fetch(`http://127.0.0.1:5000/v0/getHistory`, {
-        method: 'POST',
-        body: JSON.stringify(temp),
+    //let temp=iconBody.current;
+    //temp.ticker="META"; Nasdaq Inc
+    fetch(`http://127.0.0.1:5000/v0/add`, {
+      method: 'POST',
+      body: `{"ticker": "NDAQ"}`,
+      headers: new Headers({
+        'Content-Type': 'application/json',
+        })
+    }).then((res)=>{
+      if (!res.ok){
+        throw res;
+      }
+      fetch(`http://127.0.0.1:5000/v0/ticker/?id=NDAQ`, {
+        method: 'GET',
         headers: new Headers({
           'Content-Type': 'application/json',
         })
+      }).then((response) => {
+        if (!response.ok){
+          throw response;
+        }
+        return response.json();
+      }).then((json)=>{
+        console.log("1",json);
+        setIcon1(json);
+        return(json);
       })
-      const icon1Response = await fetch(`http://127.0.0.1:5000/v0/view?id=META`, {
+      return res;
+    });
+    
+    
+    // Icon 2
+    //temp.ticker="AAPL"; DOW Jones
+    fetch(`http://127.0.0.1:5000/v0/add`, {
+      method: 'POST',
+      body: `{"ticker": "DOW"}`,
+      headers: new Headers({
+        'Content-Type': 'application/json',
+        })
+    }).then((res)=>{
+      if (!res.ok){
+        throw res;
+      }
+      fetch(`http://127.0.0.1:5000/v0/ticker/?id=DOW`, {
         method: 'GET',
         headers: new Headers({
           'Content-Type': 'application/json',
         })
-      });
-      const icon1Json = await icon1Response.json();
-      setIcon1(icon1Json);
-      
-      // Icon 2
-      temp.ticker="AAPL";
-      fetch(`http://127.0.0.1:5000/v0/getHistory`, {
-        method: 'POST',
-        body: JSON.stringify(temp),
-        headers: new Headers({
-          'Content-Type': 'application/json',
-        })
-      });
-      const icon2Response = await fetch(`http://127.0.0.1:5000/v0/view?id=AAPL`, {
-        method: 'GET',
-        headers: new Headers({
-          'Content-Type': 'application/json',
-        })
-      });
-      const icon2Json = await icon2Response.json();
-      setIcon2(icon2Json);
-      
-      // Icon 3
-      temp.ticker="AMZN";
-      fetch(`http://127.0.0.1:5000/v0/getHistory`, {
-        method: 'POST',
-        body: JSON.stringify(temp),
-        headers: new Headers({
-          'Content-Type': 'application/json',
-        })
+      }).then((response) => {
+        if (!response.ok){
+          throw response;
+        }
+        return response.json();
+      }).then((json)=>{
+        console.log("2",json);
+        setIcon2(json);
+        return(json);
       })
-      const icon3Response = await fetch(`http://127.0.0.1:5000/v0/view?id=AMZN`, {
+      return res;
+    });
+    
+    // Icon 3
+    //temp.ticker="AMZN"; E-Mini S&P 500 Mar 23
+    fetch(`http://127.0.0.1:5000/v0/add`, {
+      method: 'POST',
+      body: `{"ticker": "CME"}`,
+      headers: new Headers({
+        'Content-Type': 'application/json',
+        })
+    }).then((res)=>{
+      if (!res.ok){
+        throw res;
+      }
+      fetch(`http://127.0.0.1:5000/v0/ticker/?id=CME`, {
         method: 'GET',
         headers: new Headers({
           'Content-Type': 'application/json',
         })
-      });
-      const icon3Json = await icon3Response.json();
-      setIcon3(icon3Json);
-      
-      // Icon 4
-      temp.ticker="NFLX";
-      fetch(`http://127.0.0.1:5000/v0/getHistory`, {
-        method: 'POST',
-        body: JSON.stringify(temp),
-        headers: new Headers({
-          'Content-Type': 'application/json',
+      }).then((response) => {
+        if (!response.ok){
+          throw response;
+        }
+        return response.json();
+      }).then((json)=>{
+        console.log("3 ",json);
+        setIcon3(json);
+        return(json);
+      })
+      return res;
+    });
+    
+    // Icon 4
+    //temp.ticker="NFLX"; 
+    fetch(`http://127.0.0.1:5000/v0/add`, {
+      method: 'POST',
+      body: `{"ticker": "AMZN"}`,
+      headers: new Headers({
+        'Content-Type': 'application/json',
         })
-      });
-      const icon4Response = await fetch(`http://127.0.0.1:5000/v0/view?id=NFLX`, {
+    }).then((res)=>{
+      if (!res.ok){
+        throw res;
+      }
+      fetch(`http://127.0.0.1:5000/v0/ticker/?id=AMZN`, {
         method: 'GET',
         headers: new Headers({
           'Content-Type': 'application/json',
         })
-      });
-      const icon4Json = await icon4Response.json();
-      setIcon4(icon4Json);
-      
-      // Icon 5
-      temp.ticker="GOOGL";
-      fetch(`http://127.0.0.1:5000/v0/getHistory`, {
-        method: 'POST',
-        body: JSON.stringify(temp),
-        headers: new Headers({
-          'Content-Type': 'application/json',
-          })
-      });
-      const icon5Response = await fetch(`http://127.0.0.1:5000/v0/view?id=GOOGL`, {
+      }).then((response) => {
+        if (!response.ok){
+          throw response;
+        }
+        return response.json();
+      }).then((json)=>{
+        console.log("4",json);
+        setIcon4(json);
+        return(json);
+      })
+      return res;
+    });
+    
+    // Icon 5
+    //temp.ticker="GOOGL";
+    fetch(`http://127.0.0.1:5000/v0/add`, {
+      method: 'POST',
+      body: `{"ticker": "GOOGL"}`,
+      headers: new Headers({
+        'Content-Type': 'application/json',
+        })
+    }).then((res)=>{
+      if (!res.ok){
+        throw res;
+      }
+      fetch(`http://127.0.0.1:5000/v0/ticker/?id=GOOGL`, {
         method: 'GET',
         headers: new Headers({
           'Content-Type': 'application/json',
         })
-      });
-      const icon5Json = await icon5Response.json();
-      setIcon5(icon5Json);
-      
-      // Icon 6
-      temp.ticker="MSFT";
-      fetch(`http://127.0.0.1:5000/v0/getHistory`, {
-        method: 'POST',
-        body: JSON.stringify(temp),
-        headers: new Headers({
-          'Content-Type': 'application/json',
-          })
-      });
-      const icon6Response = await fetch(`http://127.0.0.1:5000/v0/view?id=MSFT`, {
+      }).then((response) => {
+        if (!response.ok){
+          throw response;
+        }
+        return response.json();
+      }).then((json)=>{
+        console.log("5",json);
+        setIcon5(json);
+        return(json);
+      })
+      return res;
+    });
+    
+    // Icon 6
+    //temp.ticker="MSFT";
+    fetch(`http://127.0.0.1:5000/v0/add`, {
+      method: 'POST',
+      body: `{"ticker": "MSFT"}`,
+      headers: new Headers({
+        'Content-Type': 'application/json',
+        })
+    }).then((res)=>{
+      if (!res.ok){
+        throw res;
+      }
+      fetch(`http://127.0.0.1:5000/v0/ticker/?id=MSFT`, {
         method: 'GET',
         headers: new Headers({
           'Content-Type': 'application/json',
         })
-      });
-      const icon6Json = await icon6Response.json();
-      setIcon6(icon6Json);
-    }
-    fetchData();
+      }).then((response) => {
+        if (!response.ok){
+          throw response;
+        }
+        return response.json();
+      }).then((json)=>{
+        console.log("6",json);
+        setIcon6(json);
+        return(json);
+      })
+      return res;
+    });
+   
+  }, []);
 
-    return() =>{
-      <></>
-    };
-  },[]);
 
 
   const c1Options = {};
@@ -189,6 +284,7 @@ const Home = () => {
   
   if (c1Data.length>0){
     let c1History= c1Data[0][0][1];
+
     for (let i=c1History.length-1; i>=0; i--){
       const temp = {};
         temp.x = new Date (c1History[i].day);
@@ -209,11 +305,13 @@ const Home = () => {
     c1Options.yaxis = {};
     c1Options.yaxis.tooltip = {};
     c1Options.yaxis.tooltip.enabled = true;
+
   }
+
 
   //RENDER WEBPAGE
   return (
-    <div id="Homepage" className="overflow-auto">
+    <div className="overflow-auto">
       
       <div className="box-border h-screen " 
         style={{backgroundImage: 'url(/homeImage.jpg)',
@@ -231,57 +329,58 @@ const Home = () => {
               </p>
             </div>
             
-            <form className="nav-item" style={{paddingTop:'10px'}} action="Individual" >
+            <form className="nav-item" style={{paddingTop:'10px'}} action="Individual">
               <button className="rounded-lg bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 w-full px-4 border border-gray-400 shadow" href="/Individual">
-              TSLA, AAPL, NVDA...
+                TSLA, AAPL, NVDA...
               </button>
             </form>
           </div>
 
       </div>
-      <div className="pt-4 px-4 text-6xl">
-        Market Trends
-        <div className="flex pt-4 grid grid-row-3 grid-col-6 gap-3 h-screen" >
-          <div className=" text-lg row-span-3 col-span-4 row-start-1 min-w-full"id="chart">
-            <ReactApexChart className=" " options={c1Options} series={c1Options.series} type="candlestick" height={800} />
+      <div className=" pt-4 px-4 text-6xl" >
+          Market Trends
+        <div className="flex pt-4 grid grid-row-3 grid-col-6 gap-3 h-screen" id="chart">
+          <div className=" text-lg row-span-3 col-span-4 row-start-1 min-w-full">
+            <ReactApexChart aria-label = "apex" className=" " options={c1Options} series={c1Options.series} type="candlestick" height={800} />
           </div>
           <div className="flex p-2 rounded-lg max-h-24 border-2 border-stone-200 px-1 row-start-1">
-            <img src={Meta} className="" height="65px" width="90px"  alt=""/>
-            <div className="flex-1" >
-              <p className="text-3xl">{icon1[0][0][0]}</p> 
-              <div className="flex text-2xl">
-                ${icon1[0][0][1][0].close.toFixed(2)}&emsp;
-                <p className="" style={{color:((icon1[0][0][1][0].close/icon1[0][0][1][0].open)-1)>0?"green":"red"}}>{(100*((icon1[0][0][1][0].close/icon1[0][0][1][0].open)-1)).toFixed(2)}%</p>
-              </div>
+            <img src={Nasdaq} className="" height="65px" width="75px"  alt=""/>
+            <div className="pl-4 flex-1" >
+            <p className="text-3xl">{icon1[0][0][0]}</p> 
+            <div className="flex text-2xl">
+              ${Number(icon1[0][0][2]).toFixed(2)}&emsp;
+              <p className="" style={{color:(icon1[0][0][4][1]==='+')?"green":"red"}}>{icon1[0][0][4]}%</p>
             </div>
+      </div>
+
           </div>
           <div className="flex p-2 rounded-lg max-h-24 border-2 border-stone-200 px-1 row-start-1 text-2xl">
-            <img src={Apple} className="" height="75px" width="75px" alt=""></img>
+            <img src={DOW} className="" height="75px" width="75px" alt=""></img>
             <div className="pl-2 flex-1" >
               <p className="text-3xl">{icon2[0][0][0]}</p> 
               <div className="flex text-2xl">
-                ${icon2[0][0][1][0].close.toFixed(2)}&emsp;
-                <p className="" style={{color:((icon2[0][0][1][0].close/icon2[0][0][1][0].open)-1)>0?"green":"red"}}>{(100*((icon2[0][0][1][0].close/icon2[0][0][1][0].open)-1)).toFixed(2)}%</p>
+                ${Number(icon2[0][0][2]).toFixed(2)}&emsp;
+                <p className="" style={{color:(icon2[0][0][4][1]==='+')?"green":"red"}}>{icon2[0][0][4]}%</p>
               </div>
             </div>    
           </div>
           <div className="flex p-2 rounded-lg max-h-24 border-2 border-stone-200 px-1 row-start-2 text-2xl">
-            <img src={Amazon} className="" height="80px" width="80px" alt=""></img>
+            <img src={CME} className="" height="80px" width="80px" alt=""></img>
             <div className="pl-2 flex-1" >
               <p className="text-3xl">{icon3[0][0][0]}</p> 
               <div className="flex text-2xl">
-                ${icon3[0][0][1][0].close.toFixed(2)}&emsp;
-                <p className="" style={{color:((icon3[0][0][1][0].close/icon3[0][0][1][0].open)-1)>0?"green":"red"}}>{(100*((icon3[0][0][1][0].close/icon3[0][0][1][0].open)-1)).toFixed(2)}%</p>
+              ${Number(icon3[0][0][2]).toFixed(2)}&emsp;
+                <p className="" style={{color:(icon3[0][0][4][1]==='+')?"green":"red"}}>{icon3[0][0][4]}%</p>
               </div>
             </div>
           </div>
           <div className="flex p-2 rounded-lg max-h-24 border-2 border-stone-200 px-1 row-start-2text-2xl">
-            <img src={Netflix} className="" height="80px" width="90px" alt=""></img>
-            <div className="flex-1" >
+            <img src={Amazon} className="" height="80px" width="80px" alt=""></img>
+            <div className="pl-2 flex-1" >
               <p className="text-3xl">{icon4[0][0][0]}</p> 
               <div className="flex text-2xl">
-                ${icon4[0][0][1][0].close.toFixed(2)}&emsp;
-                <p className="" style={{color:((icon4[0][0][1][0].close/icon4[0][0][1][0].open)-1)>0?"green":"red"}}>{(100*((icon4[0][0][1][0].close/icon4[0][0][1][0].open)-1)).toFixed(2)}%</p>
+              ${Number(icon4[0][0][2]).toFixed(2)}&emsp;
+                <p className="" style={{color:(icon4[0][0][4][1]==='+')?"green":"red"}}>{icon4[0][0][4]}%</p>
               </div>
             </div>
           </div>
@@ -290,8 +389,8 @@ const Home = () => {
             <div className="pl-2 flex-1" >
               <p className="text-3xl">{icon5[0][0][0]}</p> 
               <div className="flex text-2xl">
-                ${icon5[0][0][1][0].close.toFixed(2)}&emsp;
-                <p className="" style={{color:((icon5[0][0][1][0].close/icon5[0][0][1][0].open)-1)>0?"green":"red"}}>{(100*((icon5[0][0][1][0].close/icon5[0][0][1][0].open)-1)).toFixed(2)}%</p>
+                ${Number(icon5[0][0][2]).toFixed(2)}&emsp;
+                <p className="" style={{color:(icon5[0][0][4][1]==='+')?"green":"red"}}>{icon5[0][0][4]}%</p>
               </div>
             </div>
           </div>
@@ -300,12 +399,11 @@ const Home = () => {
             <div className="pl-3 flex-1" >
               <p className="text-3xl">{icon6[0][0][0]}</p> 
               <div className="flex text-2xl">
-                ${icon6[0][0][1][0].close.toFixed(2)}&emsp;
-                <p className="" style={{color:((icon6[0][0][1][0].close/icon6[0][0][1][0].open)-1)>0?"green":"red"}}>{(100*((icon6[0][0][1][0].close/icon6[0][0][1][0].open)-1)).toFixed(2)}%</p>
+                ${Number(icon6[0][0][2]).toFixed(2)}&emsp;
+                <p className="" style={{color:(icon6[0][0][4][1]==='+')?"green":"red"}}>{icon6[0][0][4]}%</p>
               </div>
             </div>
           </div>
-          
         </div>
       </div>         
 
